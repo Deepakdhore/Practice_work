016bd4d65c3655f7c47e8c2166af20530f9097ac
Deepakdhore <87926172+Deepakdhore@users.noreply.github.com>
2025-11-05 15:14:41 +0530

    FS project

--- FS-Project/my-social-app/src/pages/OtherProfilePage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { OtherUserProfile } from "../components/OtherUserProfile/OtherUserProfile";
-
-const ProfilePage: React.FC = () => {
-  return <OtherUserProfile />;
-};
-
-export default ProfilePage;
